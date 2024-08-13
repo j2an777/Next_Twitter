17b9339a472213bdee597a6b2f7f2af3f3eb7654
j2an777 <ha991104@gmail.com>
2024-08-13 16:37:26 +0900

    feat: useSession으로 사용자 정보 있으면 트렌드 정보 렌더링

--- src/app/(afterLogin)/_component/_trendSection/TrendSection.tsx
+++ src/app/(afterLogin)/_component/_trendSection/TrendSection.tsx
@@ -1,27 +1,39 @@
 "use client";
+
 import { usePathname } from 'next/navigation';
 import Trend from '../_trend/Trend';
 import style from './trendSection.module.css';
+import { useSession } from 'next-auth/react';
 
 const TrendSection = () => {
   const pathname = usePathname();
+  const { data } = useSession();
   if (pathname === '/explore') return null;
 
+  if (data?.user) {
+    return (
+      <div className={style.trendBg}>
+          <div className={style.trend}>
+              <h3>나를 위한 트렌드</h3>
+              <Trend />
+              <Trend />
+              <Trend />
+              <Trend />
+              <Trend />
+              <Trend />
+              <Trend />
+              <Trend />
+              <Trend />
+              <Trend />
+          </div>
+      </div>
+    );
+  }
   return (
     <div className={style.trendBg}>
-        <div className={style.trend}>
-            <h3>나를 위한 트렌드</h3>
-            <Trend />
-            <Trend />
-            <Trend />
-            <Trend />
-            <Trend />
-            <Trend />
-            <Trend />
-            <Trend />
-            <Trend />
-            <Trend />
-        </div>
+      <div className={style.noTrend}>
+        트렌드를 가져올 수 없습니다.
+      </div>
     </div>
   )
 }
