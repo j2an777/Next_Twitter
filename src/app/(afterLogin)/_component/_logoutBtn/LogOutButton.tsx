05196147e546bbc800e70ccb1fa0e374f189f491
j2an777 <ha991104@gmail.com>
2024-08-13 16:40:48 +0900

    feat: 로그아웃 시 최상단 '/'으로 이동

--- src/app/(afterLogin)/_component/_logoutBtn/LogOutButton.tsx
+++ src/app/(afterLogin)/_component/_logoutBtn/LogOutButton.tsx
@@ -1,26 +1,32 @@
 "use client";
+
 import Image from 'next/image';
 import style from './logOutButton.module.css';
+import { signOut, useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 
 const LogOutButton = () => {
-    const me = { // 임시로 내 정보 있는것처럼
-        id: 'j2an777',
-        nickname: '즤니',
-        image: '/5Udwvqim.jpg'
-    }
+    const router = useRouter();
+    const { data: me } = useSession();
 
     const onLogout = () => {
-
+        signOut({ redirect: false })
+            .then(() => {
+                console.log("Redirecting to '/'");
+                router.replace('/');
+            });
     };
+
+    if (!me?.user) return null;
 
     return (
         <button className={style.logOutButton} onClick={onLogout}>
             <div className={style.logOutUserImage}>
-                <Image src={me.image} alt={me.id} width={30} height={40}/>
+                <Image src={me.user?.image as string} alt={me.user?.email as string} width={30} height={40}/>
             </div>
             <div className={style.logOutUserName}>
-                <div>{me.nickname}</div>
-                <div>@{me.id}</div>
+                <div>{me.user?.name}</div>
+                <div>@{me.user?.email}</div>
             </div>
         </button>
     )
