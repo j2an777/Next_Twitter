4c0b57bad7ce95b5fbc95260e63807063c257944
j2an777 <ha991104@gmail.com>
2024-08-16 13:08:22 +0900

    feat: hydrationBoundry client에 적용

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,26 +1,29 @@
 import PostForm from './_component/_postForm/PostForm';
-import PostItem from '../_component/_postItem/PostItem';
 import Tab from './_component/_tab/Tab';
 import style from './home.module.css';
 import TabProvider from './_component/TabProvider';
+import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
+import { getPostRecommends } from './_hooks/getPostRecommends';
+import TabDecider from './_component/_tabDecider/TabDecider';
 
-const Home = () => {
+const Home = async () => {
+    const queryClient = new QueryClient();
+    await queryClient.prefetchQuery({
+        queryKey: ['posts', 'recommends'],
+        queryFn: () => getPostRecommends(),
+    });
+
+    const dehydratedState = dehydrate(queryClient);
+
     return (
         <main className={style.main}>
-            <TabProvider>
-                <Tab />
-                <PostForm />
-                <PostItem />
-                <PostItem />
-                <PostItem />
-                <PostItem />
-                <PostItem />
-                <PostItem />
-                <PostItem />
-                <PostItem />
-                <PostItem />
-                <PostItem />
-            </TabProvider>
+            <HydrationBoundary state={dehydratedState}>
+                <TabProvider>
+                    <Tab />
+                    <PostForm />
+                    <TabDecider />
+                </TabProvider>
+            </HydrationBoundary>
         </main>
     )
 }
