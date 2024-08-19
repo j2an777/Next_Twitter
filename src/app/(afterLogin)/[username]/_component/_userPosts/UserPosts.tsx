efe473cb6ebb350ef6346591c6ba87fb598b2fd0
j2an777 <ha991104@gmail.com>
2024-08-19 17:49:38 +0900

    feat: query string의 username 값에 해당하는 게시글 정보 불러오기

---
+++ src/app/(afterLogin)/[username]/_component/_userPosts/UserPosts.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import {useQuery, useQueryClient} from "@tanstack/react-query";
+import {Post} from "@/model/Post";
+import { getUserPosts } from "../../_hooks/getUserPosts";
+import PostItem from "@/app/(afterLogin)/_component/_postItem/PostItem";
+
+type UserPostsProps = {
+  username: string;
+}
+export default function UserPosts({ username }: UserPostsProps) {
+    const { data } = useQuery<Post[], Object, Post[], [_1: string, _2: string, _3: string]>({
+      queryKey: ['posts', 'users', username],
+      queryFn: getUserPosts,
+      staleTime: 60 * 1000, // fresh -> stale, 5분이라는 기준
+      gcTime: 300 * 1000,
+    });
+
+    const queryClient = useQueryClient();
+    const user = queryClient.getQueryData(['users', username]);
+
+    if (user) {
+        return data?.map((post) => (
+            <PostItem key={post.postId} post={post} />
+        ))
+    }
+
+    return null;
+}
