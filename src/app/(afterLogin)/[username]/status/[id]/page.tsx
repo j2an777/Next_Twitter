c1b04f191f4fefdf94307c2e8b6e7f94f086d0aa
j2an777 <ha991104@gmail.com>
2024-08-20 16:52:23 +0900

    fix: 게시글 상세 페이지 hydrationboundary 감싸기 및 queryClient에 의한 prefetch 설정

--- src/app/(afterLogin)/[username]/status/[id]/page.tsx
+++ src/app/(afterLogin)/[username]/status/[id]/page.tsx
@@ -1,25 +1,46 @@
 import BackButton from '@/app/(afterLogin)/_component/_backButton/BackButton';
 import style from './singlePost.module.css';
-import PostItem from '@/app/(afterLogin)/_component/_postItem/PostItem';
-import CommentForm from './_component/CommentForm';
+import CommentForm from './_component/_commentForm/CommentForm';
+import SinglePost from './_component/_singlePost/SinglePost';
+import Comments from './_component/_comments/Comments';
+import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
+import { getSinglePost } from './_hooks/getSinglePost';
+import { getComments } from './_hooks/getComments';
 
-const SinglePost = () => {
+type SinglePostProps = {
+  params: { id: string };
+}
+
+const SinglePostSection = async ({ params }: SinglePostProps) => {
+  const { id } = params;
+
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ['posts', id],
+    queryFn: getSinglePost
+  });
+  await queryClient.prefetchQuery({
+    queryKey: ['posts', id, 'comments'],
+    queryFn: getComments
+  });
+
+  const dehydratedState = dehydrate(queryClient);
+
   return (
     <main className={style.main}>
+      <HydrationBoundary state={dehydratedState}>
         <div className={style.header}>
             <BackButton />
             <h3 className={style.headerTitle}>게시하기</h3>
         </div>
-        <PostItem />
-        <CommentForm />
-        <PostItem />
-        <PostItem />
-        <PostItem />
-        <PostItem />
-        <PostItem />
-        <PostItem />
+        <SinglePost id={id} />
+        <CommentForm id={id}/>
+        <div>
+          <Comments id={id}/>
+        </div>
+      </HydrationBoundary>
     </main>
   )
 }
 
-export default SinglePost
+export default SinglePostSection
