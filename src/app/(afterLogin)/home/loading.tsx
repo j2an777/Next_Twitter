74a09221c78b25d6a993eafe1242c5b2fa7a6202
j2an777 <ha991104@gmail.com>
2024-08-21 17:46:01 +0900

    feat: loading 컴포넌트 구현

---
+++ src/app/(afterLogin)/home/loading.tsx
@@ -0,0 +1,16 @@
+import style from './home.module.css';
+
+const Loading = () => {
+    return (
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <svg className={style.loader} height="100%" viewBox="0 0 32 32" width={40} >
+                <circle cx="16" cy="16" fill="none" r="14" strokeWidth="4"
+                    style={{stroke: 'rgb(29, 155, 240)', opacity: 0.2}}></circle>
+                <circle cx="16" cy="16" fill="none" r="14" strokeWidth="4"
+                    style={{stroke: 'rgb(29, 155, 240)', strokeDasharray: 80, strokeDashoffset: 60}}></circle>
+            </svg>
+        </div>
+    )
+}
+
+export default Loading
